e260c4b9a81418268fe87c704d9f6168ca5c9713
tatis3 <tatis3@nexon.co.kr>
2020-09-03 18:33:48 +0900

    정수형 타입 변경 및 가중치 테스트 코드 추가

--- AnnuityLottery.py
+++ AnnuityLottery.py
@@ -25,10 +25,9 @@ class AnnuityLotteryAnalysis:
                     self.NumUnitList[numUnit][num] = int(findNum)
                     num += 1
                 numUnit += 1
-        #print(self.NumUnitList)
 
     def AnalysisAll(self):
-        result = np.empty(len(self.NumUnitList))
+        result = np.zeros(len(self.NumUnitList), dtype=int)
         for i in range(1, len(self.NumUnitList)):
             result[i] = self.Analysis(i)
         print("이번주 연금 복권 번호")
@@ -36,10 +35,10 @@ class AnnuityLotteryAnalysis:
         print('십만\t| 만\t| 천\t| 백\t| 십\t| 일')
         print(f'{result[1]}\t| {result[2]}\t| {result[3]}\t| {result[4]}\t| {result[5]}\t| {result[6]}')
 
-    def Analysis(self, unitNum):
+    def Analysis(self, unitNum, test=False):
         numList = self.NumUnitList[unitNum]
-        diffNum = np.empty(len(numList))
-        weightList = np.empty(len(numList))
+        diffNum = np.zeros(10, dtype=int)
+        weightList = np.zeros(10)
         maxNum = 0
         minNum = 999
         sumDiff = 0
@@ -48,7 +47,7 @@ class AnnuityLotteryAnalysis:
                 maxNum = numList[i]
             if numList[i] < minNum:
                 minNum = numList[i]
-        maxNum += (maxNum - minNum)
+        maxNum += ((maxNum - minNum) / 2)
 
         for i in range(len(numList)):
             diffNum[i] = maxNum - numList[i]
@@ -67,9 +66,17 @@ class AnnuityLotteryAnalysis:
         print('---------------------------------------------------------------------------------')
         print(f'{numList[0]}\t| {numList[1]}\t| {numList[2]}\t| {numList[3]}\t| {numList[4]}\t| {numList[5]}\t| {numList[6]}\t| {numList[7]}\t| {numList[8]}\t| {numList[9]}')
         print(f'{weightList[0]:.3f}\t| {weightList[1]:.3f}\t| {weightList[2]:.3f}\t| {weightList[3]:.3f}\t| {weightList[4]:.3f}\t| {weightList[5]:.3f}\t| {weightList[6]:.3f}\t| {weightList[7]:.3f}\t| {weightList[8]:.3f}\t| {weightList[9]:.3f}')
+
+        # 테스트
+        if test == True:
+            testL = np.zeros(10, dtype=int)
+            rt = np.random.choice(v, 1000, p=weightList)
+            for i in rt:
+                testL[i] += 1
+            print(f'{testL[0]}\t| {testL[1]}\t| {testL[2]}\t| {testL[3]}\t| {testL[4]}\t| {testL[5]}\t| {testL[6]}\t| {testL[7]}\t| {testL[8]}\t| {testL[9]}')
+
         print(f'추출값: {r}')
         print('')
-
         return r
 
 if __name__ == "__main__":
